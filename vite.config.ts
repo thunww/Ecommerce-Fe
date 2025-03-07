c50bcd33baefcb6babb228067fcadfdc7f1e291b
Tran Gia Than <trangiathandz2004@gmail.com>
2025-03-07 09:24:55 +0700

    fe hihi

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173, // Nếu muốn cố định cổng
+    open: true,  // Tự động mở trình duyệt khi chạy
+  },
+});
